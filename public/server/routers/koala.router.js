f016dfb63fd1e1e6066f093eb5886693cfd3317a
JD Ghuman <jdsghuman@gmail.com>
2019-01-24 11:02:21 -0600

    Add POST route

--- public/server/routers/koala.router.js
+++ public/server/routers/koala.router.js
@@ -16,3 +16,20 @@ const koalaSchema = new Schema({
 const Koala = mongoose.model('Koala', koalaSchema);
 
 // Setup a GET route
+router.get('/', (req, res) => {
+  Koala.find({})
+  .then(results => {
+    console.log(results);
+    res.send(results);
+  })
+  .catch(error => {
+    console.log(`Error making GET db query ${error}`);
+    res.sendStatus(500);
+  })
+})
+
+// Setup a POST route
+router.post('/', (req, res) => {
+  const newKoala = req.body;
+  console.log('req.body ')
+})
