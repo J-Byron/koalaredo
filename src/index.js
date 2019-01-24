b31ef3c0805afd55a2eae98568a4dc7b3baded67
John Worley <john.aver.worley@gmail.com>
2019-01-24 14:32:48 -0600

    functionality of redux index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
+
+import rootReducer from './Redux/Reducers/index'; // imports ./redux/reducers/index.js
+import rootSaga from './Redux/Sagas/index'; // imports ./redux/sagas/index.js
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewareList = process.env.NODE_ENV === 'development' ?
+    [sagaMiddleware, logger] :
+    [sagaMiddleware];
+
+sagaMiddleware.run(rootSaga);
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(...middlewareList),
+)
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
