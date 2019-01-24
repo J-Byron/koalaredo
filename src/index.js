f88d7a7b42492b08b9c8fd64e0427f64f420e3dd
John Worley <john.aver.worley@gmail.com>
2019-01-24 14:44:57 -0600

    index

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
 
-import rootReducer from './Redux/Reducers/index'; // imports ./redux/reducers/index.js
-import rootSaga from './Redux/Sagas/index'; // imports ./redux/sagas/index.js
+import rootReducer from './Redux/Reducers/'; // imports ./redux/reducers/index.js
+import rootSaga from './Redux/Sagas/'; // imports ./redux/sagas/index.js
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,15 +19,22 @@ const middlewareList = process.env.NODE_ENV === 'development' ?
     [sagaMiddleware, logger] :
     [sagaMiddleware];
 
-sagaMiddleware.run(rootSaga);
+
 
 const store = createStore(
     rootReducer,
     applyMiddleware(...middlewareList),
-)
+);
+
+sagaMiddleware.run(rootSaga)
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
